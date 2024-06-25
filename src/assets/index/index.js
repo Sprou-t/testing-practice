098a642381e7c4c480a8d03c10998b8ee26519c5
Sprou-t <150780574+Sprou-t@users.noreply.github.com>
2024-06-25 23:23:58 +0800

    finished JS testing practice

--- src/assets/index/index.js
+++ src/assets/index/index.js
@@ -1,8 +1,58 @@
 import './index.css';
 import './reset.css';
 
-function sum(a, b) {
-	return a + b;
+function wordConverter(word) {
+	function capitalise(word) {
+		const firstLetter = word.charAt(0);
+		const remainingWord = word.substring(1);
+		const capitaliseWord = firstLetter.toUpperCase() + remainingWord;
+		return capitaliseWord;
+	}
+
+	function reverseString(word) {
+		const strRev = word.split('').reverse().join('');
+		return strRev;
+	}
+
+	return { capitalise, reverseString };
+}
+function calculator(a, b) {
+	function add(a, b) {
+		return a + b;
+	}
+
+	function subtract(a, b) {
+		return a - b;
+	}
+
+	function multiply(a, b) {
+		return a * b;
+	}
+
+	return { add, subtract, multiply };
 }
 
-export default sum;
+function arrayAnalyser(arrObj) {
+	function getAverage(arrObj) {
+		const total = arrObj.reduce((sum, currentItem) => sum + currentItem, 0);
+		const itemNum = arrObj.length;
+		const avg = total / itemNum;
+		return avg;
+	}
+
+	function getMin(arrObj) {
+		return Math.min(...arrObj);
+	}
+
+	function getMax(arrObj) {
+		return Math.max(...arrObj);
+	}
+
+	function getArrayLength(arrObj) {
+		return arrObj.length;
+	}
+
+	return { getAverage, getMin, getMax, getArrayLength };
+}
+export default calculator;
+export { wordConverter, arrayAnalyser };
--- src/assets/index/index.test.js
+++ src/assets/index/index.test.js
@@ -1,10 +1,42 @@
-// use regex to test for
-// capitalise
-// reverseString
+import calculator, { wordConverter, arrayAnalyser } from './index';
 
-// use expect.tobe for calculator
-import sum from './index';
+test('wordConverter', () => {
+	const wordCvt = wordConverter();
+	// capitalise
+	expect(wordCvt.capitalise('alan')).toMatch('Alan');
 
-test('adds 1 + 2 to equal 3', () => {
-	expect(sum(1, 2)).toBe(3);
+	// reverseString
+	expect(wordCvt.reverseString('Friend')).toMatch('dneirF');
+	expect(wordCvt.reverseString('Oop')).toMatch('poO');
+});
+
+test('calculator', () => {
+	const calc = calculator(); // Create an instance of the calculator
+
+	// add
+	expect(calc.add(2, 3)).toBe(5);
+
+	// subtract
+	expect(calc.subtract(6, 2)).toBe(4);
+
+	// multiply
+	expect(calc.multiply(7, 2)).toBe(14);
+});
+
+// test an array which returns the average, min, max of its values + length of array
+test('analyseArray', () => {
+	const arrObj = [1, 8, 3, 4, 2, 6];
+	const arrayResult = arrayAnalyser();
+
+	// average
+	expect(arrayResult.getAverage(arrObj)).toBe(4);
+
+	// min
+	expect(arrayResult.getMin(arrObj)).toBe(1);
+
+	// max
+	expect(arrayResult.getMax(arrObj)).toBe(8);
+
+	// length
+	expect(arrayResult.getArrayLength(arrObj)).toBe(6);
 });
