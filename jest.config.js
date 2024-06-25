fb45fb5f7fa5e5acc4b4ec4bb9409f899cd80c5a
Sprou-t <150780574+Sprou-t@users.noreply.github.com>
2024-06-26 01:37:17 +0800

    made further configuration for jest

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-	testEnvironment: 'node',
+	testEnvironment: 'jest-environment-jsdom',
 	transform: {
 		'^.+\\.jsx?$': 'babel-jest',
 	},
 	moduleNameMapper: {
 		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
 			'<rootDir>/__mocks__/fileMock.js',
-		'\\.(css|less)$': '<rootDir>/mocks/styleMock.js',
+		'\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
 		'^config$': '<rootDir>/configs/app-config.js',
 	},
 	modulePaths: ['/shared/vendor/modules'],
